b6e793009e97b3684793ec7d25dd533261bcc201
steindak <david.stein@novartis.com>
2019-07-31 13:49:22 -0400

    added post service

---
+++ services/post-service.js
@@ -0,0 +1,33 @@
+module.exports = app => {
+
+    const postModel = require('../models/post/post-model');
+
+    findAllPosts = (_, res) =>
+        postModel.findAllUsers()
+            .then(posts => {
+                res.send(posts);
+            });
+
+    createPost = (req, res) => 
+        postModel.createPost(req.body);
+
+    updatePost = (req, res) =>
+        postModel.updatePost(req.body)
+            .then(post => res.send(post));
+
+    deletePost = (req, res) => {
+        const postId = req.body;
+        postModel.deletePost(postId)
+        res.send(200);
+    }
+
+    findPostById = (req, res) =>
+        postModel.findPostById(req.params.postId)
+            .then(post => res.send(post));
+  
+    app.get ('/api/post', findAllPosts);
+    app.post('/api/post', createPost);
+    app.put('/api/post/:postId', updatePost);
+    app.delete('/api/post/:postId', deletePost);
+    app.get('/api/post/:userId', findPostById);
+};
