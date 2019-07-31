0597097c302f1eb541df327c040e2d738d1a9faa
steindak <david.stein@novartis.com>
2019-07-31 13:06:35 -0400

    model for post documents

---
+++ models/post/post-model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const postSchema = require('./post-schema');
+
+const postModel = mongoose.model('PostModel', postSchema);
+
+findAllPosts = () =>
+    postModel.find({});
+
+findPostById = postId => 
+    postModel.findById(postId);
+
+createPost = (post) =>
+    postModel.create(post);
+
+updatePost = (post, date) =>
+    postModel.update({
+        _id: post._id
+    }, {
+        title: post.title,
+        body: post.body,
+        updated: date
+    });
+
+deletePost = (postId) =>
+    postModel.delete({_id: postId});
+
+module.exports = {
+  findAllPosts,
+  findPostById,
+  createPost,
+  updatePost,
+  deletePost,
+};
