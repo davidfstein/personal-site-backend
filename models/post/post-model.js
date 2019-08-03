9310824b48490517c433de055137a998966e1b98
steindak <david.stein@novartis.com>
2019-08-03 00:24:39 -0400

    remove

--- models/post/post-model.js
+++ models/post/post-model.js
@@ -22,7 +22,7 @@ updatePost = (post, date) =>
     });
 
 deletePost = (postId) =>
-    postModel.remove({_id: mongoose.Types.ObjectId(postId)});
+    postModel.remove({_id: mongoose.Types.ObjectId(String(postId))});
 
 module.exports = {
   findAllPosts,
